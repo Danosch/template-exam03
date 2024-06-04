0b925a576ecdb440c650e4363bb7197486b79ee7
Danosch <dennis.flegler@student.fhws.de>
2024-06-04 16:57:48 +0200

    added states for Module

---
+++ src/main/java/de/fhws/fiw/fds/suttondemo/server/api/states/modules/DeleteSingleModule.java
@@ -0,0 +1,33 @@
+package de.fhws.fiw.fds.suttondemo.server.api.states.modules;
+
+import de.fhws.fiw.fds.sutton.server.api.serviceAdapters.responseAdapter.JerseyResponse;
+import de.fhws.fiw.fds.sutton.server.api.services.ServiceContext;
+import de.fhws.fiw.fds.sutton.server.api.states.delete.AbstractDeleteState;
+import de.fhws.fiw.fds.sutton.server.database.results.NoContentResult;
+import de.fhws.fiw.fds.sutton.server.database.results.SingleModelResult;
+import de.fhws.fiw.fds.suttondemo.server.api.models.Module;
+import de.fhws.fiw.fds.suttondemo.server.database.DaoFactory;
+import jakarta.ws.rs.core.Response;
+
+public class DeleteSingleModule extends AbstractDeleteState<Response, Module> {
+
+    public DeleteSingleModule(ServiceContext serviceContext, long modelIdToDelete) {
+        super(serviceContext, modelIdToDelete);
+        this.suttonResponse = new JerseyResponse<>();
+    }
+
+    @Override
+    protected SingleModelResult<Module> loadModel() {
+        return DaoFactory.getInstance().getModuleDao().readById(this.modelIdToDelete);
+    }
+
+    @Override
+    protected NoContentResult deleteModel() {
+        return DaoFactory.getInstance().getModuleDao().delete(this.modelIdToDelete);
+    }
+
+    @Override
+    protected void defineTransitionLinks() {
+
+    }
+}
